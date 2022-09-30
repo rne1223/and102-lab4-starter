f091485c7f4f518449c8c91056396391d84f91f8
rne1223 <rne1223@gmail.com>
2022-09-30 12:48:49 -0700

    Finished Lab

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
+import org.json.JSONObject
 
 fun createJson() = Json {
     isLenient = true
@@ -25,6 +28,7 @@ private const val ARTICLE_SEARCH_URL =
     "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${SEARCH_API_KEY}"
 
 class MainActivity : AppCompatActivity() {
+    private val articles = mutableListOf<Article>()
     private lateinit var articlesRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
@@ -36,20 +40,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         articlesRecyclerView = findViewById(R.id.articles)
-        // TODO: Set up ArticleAdapter with articles
 
         articlesRecyclerView.layoutManager = LinearLayoutManager(this).also {
+
             val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
             articlesRecyclerView.addItemDecoration(dividerItemDecoration)
         }
 
+        val articleAdapter = ArticleAdapter(this, articles)
+        articlesRecyclerView.adapter = articleAdapter
+
+
         val client = AsyncHttpClient()
         client.get(ARTICLE_SEARCH_URL, object : JsonHttpResponseHandler() {
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
+            override fun onFailure( statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
             ) {
                 Log.e(TAG, "Failed to fetch articles: $statusCode")
             }
@@ -58,10 +62,29 @@ class MainActivity : AppCompatActivity() {
                 Log.i(TAG, "Successfully fetched articles: $json")
                 try {
                     // TODO: Create the parsedJSON
+                    val parsedJson = createJson().decodeFromString(
+                        SearchNewsResponse.serializer(),
+                        json.jsonObject.toString()
+                    )
 
-                    // TODO: Do something with the returned json (contains article information)
+                    // TODO: Save the articles
+                    parsedJson.response?.docs?.let { list ->
+                        articles.addAll(list)
 
-                    // TODO: Save the articles and reload the screen
+                        // TODO: Reload the screen
+                        articleAdapter.notifyDataSetChanged()
+                    }
+
+
+                    /** Previous way of dealing with json
+                    val resultsJSON : JSONObject = json.jsonObject.get("response") as JSONObject
+                    val articleRawJson: String = resultsJSON.get("docs").toString()
+                    val gson = Gson()
+                    val arrayArticleType = object: TypeToken<List<Article>>() {}.type
+                    val articles: List<Article> = gson.fromJson(articleRawJson, arrayArticleType)
+
+                    Log.v("MainActivity", articles.toString())
+                    */
 
                 } catch (e: JSONException) {
                     Log.e(TAG, "Exception: $e")
